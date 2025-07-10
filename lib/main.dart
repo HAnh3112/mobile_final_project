fd6874b3cbdaf0358d8a6ef40b02c7e1e1490426
hoangnguyengiap <157078247+hoangnguyengiap@users.noreply.github.com>
2025-07-10 10:22:27 +0700

    sửa lại file main cho gọn hơn

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:final_project/BudgetPlanning.dart';
 import 'package:final_project/EditScreen.dart';
-
-// void main() {
-//   runApp(MaterialApp(home: BudgetPlanning()));
-// }
-
 import 'screens/auth_screen.dart';
 import 'screens/category_management_screen.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Demo App',
 
-      //  Đổi giữa 2 màn hình tại đây:
-      //home: const AuthScreen(), // màn hình đăng nhập
-      home: CategoryManagementScreen(), // màn hình danh mục
-    );
-  }
+      // Chọn màn hình muốn chạy ở đây:
+      home: const AuthScreen(),
+      //home: BudgetPlanning(),
+      //home: EditScreen(),
+      //home: CategoryManagementScreen(),
+    ),
+  );
 }
