02e0179866bf9a099bfd337940baf0d37d43ee88
HAnh3112 <haianh311205@gmail.com>
2025-07-10 11:48:02 +0700

    Temporary rearrange project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:final_project/BudgetPlanning.dart';
-import 'package:final_project/EditScreen.dart';
-import 'screens/auth_screen.dart';
-import 'screens/category_management_screen.dart';
-import 'screens/transaction_history_screen.dart';
+import 'package:final_project/BudgetPlanningScreen_HaiAnh/BudgetPlanning.dart';
+import 'package:final_project/BudgetPlanningScreen_HaiAnh/EditScreen.dart';
+import 'screens_Giap/auth_screen.dart';
+import 'screens_Giap/category_management_screen.dart';
+import 'screens_Giap/transaction_history_screen.dart';
 
 void main() {
   runApp(
